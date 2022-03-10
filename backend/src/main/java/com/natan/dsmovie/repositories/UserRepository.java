5934425770804d28d9deeb23e709581fc266dcd5
Natan melo <natan01melo@hotmail.com>
2022-03-09 21:19:51 -0300

    Save Score [modified]

---
+++ backend/src/main/java/com/natan/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.natan.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.natan.dsmovie.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+}
